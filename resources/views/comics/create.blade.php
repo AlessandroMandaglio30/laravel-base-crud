aba56a063e38518352f13808aeea714cf8b5640b
Alessandro Mandaglio <ale.manda.30@gmail.com>
2022-02-09 22:14:45 +0100

    add create

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,36 @@
+@extends('layout.base')
+
+@section('h1')
+Aggiungi un nuovo fumetto
+@endsection
+
+@section('pageContent')
+<form class="container py-2">
+   <div class="form-group">
+        <label for="title">Titolo</label>
+        <input type="text" class="form-control" id="title" name="title" placeholder="Inserisci il titolo">
+    </div>
+    <div class="form-group">
+        <label for="series">Serie</label>
+        <input type="text" class="form-control" id="series" name="series" placeholder="Inserisci la saga">
+    </div>
+    <div class="form-group">
+        <label for="type">Tipo fumetto</label>
+        <input type="text" class="form-control" class="type" id="type" name="type" placeholder="Inserisci tipologia">
+    </div>
+    <div class="form-group">
+        <label for="price">Prezzo</label>
+        <input type="number" class="form-control" id="price" name="price" placeholder="Inserisci il prezzo">
+    </div>
+    <div class="form-group">
+        <label for="sale_date">Data di pubblicazione</label>
+        <input type="date" class="form-control" id="sale_date" name="sale_date" placeholder="Inserisci la data di pubblicazione">
+    </div>
+    <div class="form-group">
+        <label for="description">Descrizione</label>
+        <textarea name="description"class="form-control" id="description" cols="30" rows="10" placeholder="Trama"></textarea>
+    </div>
+    <a href="{{route('comics.index')}}"><button type="button" class="btn btn-primary">Torna indietro</button></a>
+    <button type="submit" class="btn btn-primary">Aggiungi fumetto</button>
+</form>
+@endsection
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,7 +1,8 @@
 @extends('layout.base')
 
 @section('pageContent')
-<h1>Comics</h1>
+<h1 class="text-center py-2">Comics</h1>
+
 <table class="table table-dark">
     <thead>
       <tr>
@@ -11,6 +12,7 @@
         <th scope="col">Series</th>
         <th scope="col">Sale Date</th>
         <th scope="col">Type</th>
+        <th scope="col">Action</th>
       </tr>
     </thead>
     <tbody>
@@ -23,8 +25,15 @@
         <td>{{$comic->series}}</td>
         <td>{{$comic->sale_date}}</td>
         <td>{{$comic->type}}</td>
+          <td>
+            <a href="{{route("comics.show", $comic->id)}}"><button type="button" class="btn btn-success">Info</button></a>
+        </td>
       </tr>
       @endforeach
     </tbody>
   </table>
+  <div class="mx-4">
+      <a href="{{route("comics.create")}}"><button type="button" class="btn btn-success">Nuovo Fumetto</button></a>
+  </div>
+
 @endsection
